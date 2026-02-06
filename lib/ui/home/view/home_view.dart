46415edf366619241dc17be94d5ca3843b3daba5
om1cael <188113439+om1cael@users.noreply.github.com>
2026-02-06 18:43:21 -0300

    feat: add appbar

--- lib/ui/home/view/home_view.dart
+++ lib/ui/home/view/home_view.dart
@@ -8,12 +8,13 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text('CineTrace'),),
       body: SafeArea(
         minimum: EdgeInsets.all(12.0),
         child: Consumer(
           builder: (context, ref, _) {
             final movieState = ref.watch(homeViewModelAsyncProvider);
-
+        
             return movieState.when(
               data: (movies) => GridView.builder(
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -22,7 +23,7 @@ class HomeView extends StatelessWidget {
                 itemCount: movies.length,
                 itemBuilder: (_, id) {
                   final movie = movies[id];
-
+        
                   return Card(
                     child: Padding(
                       padding: const EdgeInsets.all(8.0),
