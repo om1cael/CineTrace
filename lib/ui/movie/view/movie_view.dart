a6f90a23987a0283b878c766e8cca554ab3ef070
om1cael <188113439+om1cael@users.noreply.github.com>
2026-02-06 22:25:27 -0300

    feat: implement basic movie page

--- lib/ui/movie/view/movie_view.dart
+++ lib/ui/movie/view/movie_view.dart
@@ -1,6 +1,10 @@
+import 'package:cinetrace/ui/core/connection_error.dart';
+import 'package:cinetrace/ui/movie/view_model/movie_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
-class MovieView extends StatelessWidget {
+class MovieView extends ConsumerWidget {
   const MovieView({
     super.key,
     required this.movieId,
@@ -9,9 +13,46 @@ class MovieView extends StatelessWidget {
   final String? movieId;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final movie = ref.watch(movieViewModelProvider(int.parse(movieId!)));
+
     return Scaffold(
-      body: const Placeholder(),
+      appBar: AppBar(
+        title: Text('Movie'),
+        leading: IconButton(
+          onPressed: () => context.go('/'), 
+          icon: Icon(Icons.arrow_back),
+        ),
+      ),
+      body: SafeArea(
+        minimum: EdgeInsets.all(16.0),
+        child: movie.when(
+          data: (data) {
+            return Center(
+              child: Column(
+                children: [
+                  Text(data.title, style: TextStyle(fontSize: 24, fontWeight: .bold),),
+                  Text(data.overview, textAlign: .center,),
+                  SizedBox(height: 16.0,),
+                  Row(
+                    mainAxisAlignment: .center,
+                    children: [
+                      Text('Released ${data.releaseDate.toString()}')
+                    ],
+                  ),
+                  Divider(),
+                ],
+              ),
+            );
+          },
+          error: (error, stack) => Column(
+            children: [
+              ConnectionError(errorMessage: error.toString()),
+            ],
+          ),
+          loading: () => Center(child: CircularProgressIndicator()),
+        )
+      )
     );
   }
 }
---
+++ lib/ui/movie/view_model/movie_view_model.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+
+import 'package:cinetrace/data/repositories/movie_repository_impl.dart';
+import 'package:cinetrace/domain/entities/movie_entity.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final movieViewModelProvider = 
+  AsyncNotifierProvider.family<MovieViewModel, MovieEntity, int>(MovieViewModel.new);
+
+class MovieViewModel extends AsyncNotifier<MovieEntity> {
+  MovieViewModel(this.movieId);
+  final int movieId;
+
+  @override
+  FutureOr<MovieEntity> build() {
+    return getMovieById(movieId);
+  }
+
+  Future<MovieEntity> getMovieById(int id) async {
+    return await ref.read(movieRepositoryProvider).getMovieById(id);
+  }
+}
